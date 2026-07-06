2e88e9a0b0464b12891dfa9a2ab0e7619465b2fd
LiYunkai <yunkai.li.720@gmail.com>
2026-07-06 17:16:33 +0800

    Sync server database snapshot

--- scripts/pull-server-db.sh
+++ scripts/pull-server-db.sh
@@ -8,6 +8,10 @@ REMOTE_DB="${VOYAGEPLANNER_REMOTE_DB:-/data/voyageplanner.db}"
 REMOTE_UPLOADS="${VOYAGEPLANNER_REMOTE_UPLOADS:-/data/uploads}"
 LOCAL_DB="${VOYAGEPLANNER_LOCAL_DB:-backend/voyageplanner.db}"
 LOCAL_UPLOADS="${VOYAGEPLANNER_LOCAL_UPLOADS:-backend/uploads}"
+SCP_ARGS=()
+if [[ "${VOYAGEPLANNER_SCP_LEGACY:-1}" != "0" ]]; then
+  SCP_ARGS+=("-O")
+fi
 
 timestamp="$(date +%Y%m%d-%H%M%S)"
 remote_tmp="/tmp/voyageplanner-db-${timestamp}.db"
@@ -29,13 +33,13 @@ fi
 ssh -p "$SSH_PORT" "$SSH_TARGET" \
   "docker cp ${REMOTE_CONTAINER}:${REMOTE_DB} ${remote_tmp}"
 
-scp -P "$SSH_PORT" "$SSH_TARGET:${remote_tmp}" "$LOCAL_DB"
+scp "${SCP_ARGS[@]}" -P "$SSH_PORT" "$SSH_TARGET:${remote_tmp}" "$LOCAL_DB"
 
 ssh -p "$SSH_PORT" "$SSH_TARGET" \
   "rm -rf ${remote_uploads_tmp} && docker cp ${REMOTE_CONTAINER}:${REMOTE_UPLOADS} ${remote_uploads_tmp}"
 
 rm -rf "$LOCAL_UPLOADS"
-scp -P "$SSH_PORT" -r "$SSH_TARGET:${remote_uploads_tmp}" "$LOCAL_UPLOADS"
+scp "${SCP_ARGS[@]}" -P "$SSH_PORT" -r "$SSH_TARGET:${remote_uploads_tmp}" "$LOCAL_UPLOADS"
 
 ssh -p "$SSH_PORT" "$SSH_TARGET" "rm -f ${remote_tmp} && rm -rf ${remote_uploads_tmp}"
 
